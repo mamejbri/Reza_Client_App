84269358bffd5fe1bf8b5cfd43c420143380bdc1
Quartium <firas404@gmail.com>
2025-03-31 14:58:29 +0100

    Add global styles and integrate icon library

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,36 @@ module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}",
     "./screens/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
+    "./components/**/*.{js,jsx,ts,tsx}",
+    './global.css',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        danger: '#C53334',
+        neutral: '#F7F7F7',
+        black: '#000000',
+        white: '#FFFFFF',
+      },
+      spacing: {
+        10: '10px',
+        17: '17px',
+        42: '42px',
+        52: '52px',
+        55: '55px',
+        72: '72px',
+      },
+      borderRadius: {
+        md: '16px',
+      },
+      backgroundColor: {
+        'black-3': '#0000000A',
+      },
+      fontSize: {
+        base: ['16px', '20px'],
+        lg: ['18px', '22px'],
+      },
+    },
   },
   presets: [require("nativewind/preset")],
 };
